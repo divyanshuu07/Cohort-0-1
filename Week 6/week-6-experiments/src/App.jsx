70014f4d23f8802929c6fc076805c06bfb98407c
divyanshuu07 <divyanshu9131@outlook.com>
2024-07-08 16:35:21 +0530

    Updating App.jsx

--- Week 6/week-6-experiments/src/App.jsx
+++ Week 6/week-6-experiments/src/App.jsx
@@ -3,19 +3,26 @@ import { useState } from "react"
 
 function App() {
 
+  return (
+    <>
+    <HeaderWithButton/>
+      <Header title="divyanshu2"></Header>
+    </>
+  )
+}
+
+function HeaderWithButton(){
+
   const [title,setTitle] =useState("My name is Divyanshu Singh");
 
   function updateTitle(){
     setTitle("my name is " +Math.random())
   }
 
-  return (
-    <>
+  return <div>
     <button onClick={updateTitle}>Update the title</button>
-      <Header title={title}></Header>
-      <Header title="divyanshu2"></Header>
-    </>
-  )
+    <Header title={title}></Header>
+  </div>
 }
 
 function Header({title}){
